ce959b665df6a7d02fcc5fff4bcc8809bbe59dbb
darkojelic <webismo@gmail.com>
2025-06-16 00:13:43 +0200

    Single Product

---
+++ themes/healthcare-senior-living/woocommerce/content-product-slider.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying product content within the related products slider
+ *
+ * This is a copy of content-product.php, for customizations in the related products slider.
+ *
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.4.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+// Check if the product is a valid WooCommerce product and ensure its visibility before proceeding.
+if ( ! is_a( $product, WC_Product::class ) || ! $product->is_visible() ) {
+	return;
+}
+?>
+<div <?php wc_product_class( '', $product ); ?>>
+	<?php
+	/**
+	 * Hook: woocommerce_before_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_open - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item' );
+	/**
+	 * Hook: woocommerce_before_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_show_product_loop_sale_flash - 10
+	 * @hooked woocommerce_template_loop_product_thumbnail - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item_title' );
+	/**
+	 * Hook: woocommerce_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_product_title - 10
+	 */
+	do_action( 'woocommerce_shop_loop_item_title' );
+	// Display WooCommerce SKU
+	if ( $product && $product->get_sku() ) {
+		echo '<div class="slider-product-meta"><strong>' . esc_html( $product->get_sku() ) . '</strong></div>';
+		// Show _hsl_size metabox variable below SKU
+		$hsl_size = get_post_meta( $product->get_id(), '_hsl_size', true );
+		if ( $hsl_size ) {
+			echo '<div class="slider-product-meta">' . esc_html( $hsl_size ) . '</div>';
+		}
+	}
+	/**
+	 * Hook: woocommerce_after_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_rating - 5
+	 * @hooked woocommerce_template_loop_price - 10
+	 */
+	// do_action( 'woocommerce_after_shop_loop_item_title' );
+echo "</div>";  
+
+
+	/**
+	 * Hook: woocommerce_after_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_close - 5
+	 * @hooked woocommerce_template_loop_add_to_cart - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item' );
+	?>
+</div>
