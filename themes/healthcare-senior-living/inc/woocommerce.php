555f93185bb57ef44547da146289e6b9b654336a
mightyrockca <47499468+mightyrockca@users.noreply.github.com>
2026-01-07 16:21:37 -0500

    remove detail link

--- themes/healthcare-senior-living/inc/woocommerce.php
+++ themes/healthcare-senior-living/inc/woocommerce.php
@@ -48,8 +48,8 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loo
 /**
  * Link title in loop
  */
-add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 9 );
-add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 22 );
+#add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 9 );
+#add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 22 );
 
 /**
  * Add loop container
